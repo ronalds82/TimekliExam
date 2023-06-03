381f96bf846f9c80ef795c5afac6fdf728b696f0
ronaldsturnis <ronalds.turnis@visma.com>
2023-06-03 15:56:59 +0300

    Added new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent { }
+export class AppComponent {
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('lv');
+    translate.use('lv');
+  }
+}
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navigation',
@@ -9,11 +10,25 @@ export class NavigationComponent {
   isResponsive = false;
   isDropdownOpen = false;
 
+  constructor(private translate: TranslateService) { }
+
+  get isLatvianEnabled(): boolean {
+    return this.translate.currentLang === 'lv';
+  }
+
+  get isEnglishEnabled(): boolean {
+    return this.translate.currentLang === 'en';
+  }
+
   toggleResponsive(): void {
     this.isResponsive = !this.isResponsive;
   }
 
   toggleDropdown(): void {
     this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  changeLanguage(language: string): void {
+    this.translate.use(language);
   }
 }
